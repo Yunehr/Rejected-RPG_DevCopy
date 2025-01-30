8e2f0f5007c70cdbe35e176260a5cc6ef8d1456a
Mromano06 <matbat2205@gmail.com>
2025-01-30 14:43:34 -0500

    Added a title screen

--- CSCN71030 - Project 2/mainMenu.c
+++ CSCN71030 - Project 2/mainMenu.c
@@ -8,16 +8,21 @@
 // Matthew Romano - CSCN71030_Section2_Group3 - Main Menu Implenentation
 
 // so far not returning anything (should be fine)
+// TODO: file name for the load file must be added into perameters
+// (from argv thingy, just cant remember how to do that rn)
 void mainMenu() {
 	// needs three options Exit Game (0), Create New Save File (1), Load Save File (2)
 	int selection = 0;
 
-	printf("THIS IS THE MAIN MENU");
-	printf("\n\nNEW GAME = 1\n\nLOAD GAME = 2\n\nEXIT GAME = 0\n\n");			// base for the menu options
+	printf(" U   U  N   N  TTTTT  IIIII  TTTTT  L      EEEEE  DDDD     RRRR    PPPP    GGG \n");
+	printf(" U   U  NN  N    T      I      T    L      E      D   D    R   R   P   P  G   \n");
+	printf(" U   U  N N N    T      I      T    L      EEEE   D   D    RRRR    PPPP   G  GG\n");
+	printf(" U   U  N  NN    T      I      T    L      E      D   D    R  R    P      G   G\n");
+	printf("  UUU   N   N    T    IIIII    T    LLLLL  EEEEE  DDDD     R   R   P       GGG");
+	printf("\n\n1 FOR NEW GAME\n\n2 TO LOAD GAME\n\n3 TO EXIT GAME\n\n");			// base for the menu options
 	do {
 		printf("SELECTION: ");
 		scanf("%d", &selection);
-
 		while (selection > 2 || selection < 0) {								// input validation
 			printf("\n\nINVALID INPUT, PLEASE ENTER NEW SELECTION: ");
 			scanf("%d", &selection);
@@ -31,7 +36,6 @@ void mainMenu() {
 		scanf("%d", &selection);											// selection is good to use again 
 			if (selection == 1)							
 				newGame();	// creates a new game file
-
 		break;
 	case 2:
 		loadGame("TempFileName.txt");	// loads old file
