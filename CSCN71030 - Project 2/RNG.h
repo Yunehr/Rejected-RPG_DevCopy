61dccd0cc94a1d5dad0d4c01a83cd503cc97b507
Ryan H. <Ryanhackbart@gmail.com>
2025-01-30 11:29:16 -0500

    random number generator completed

---
+++ CSCN71030 - Project 2/RNG.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Ryan Hackabrt - CSCN7130_Section2_Group3 - RNG Events API
+
+// srand(time(NULL));	Must be input once game is started for RNG to work properly (otherwise will be the same set of numbers)
+
+#include <stdlib.h>
+#include <time.h>
+
+// random number generator function
+int RNG(int maxVal, int mod);
