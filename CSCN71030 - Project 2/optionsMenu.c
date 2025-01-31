550eb2671361cb8842c8bbab972d24b6413080d1
Mromano06 <matbat2205@gmail.com>
2025-01-31 08:26:05 -0500

    added fucntions for options menu

---
+++ CSCN71030 - Project 2/optionsMenu.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Matthew Romano - CSCN71030_Section2_Group3 - Options Menu Implementation
+
+void optionsMenu() {
+
+	printf("\n\n1 BACK TO GAME\n\n2 SAVE GAME\n\n3 LOAD GAME\n\n0 MAIN MENU");
+}
---
+++ CSCN71030 - Project 2/optionsMenu.h
@@ -0,0 +1,5 @@
+#pragma once
+
+// Matthew Romano - CSCN71030_Section2_Group3 - Options Menu Header File
+
+void optionsMenu();
