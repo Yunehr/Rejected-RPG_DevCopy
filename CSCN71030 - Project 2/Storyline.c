aecee7cb060eee4c25ad33b15aea85d10fa68ded
Leanne Boulton <leanne.elizabeth@gmail.com>
2025-02-06 10:21:26 -0500

    Storyline Functions template started, tree in begining created

---
+++ CSCN71030 - Project 2/Storyline.c
@@ -0,0 +1,31 @@
+#include "Storyline.h"
+
+void storyBegins() {
+        printf("          # #### ####  \n");
+        printf("       ### \/#|### |/####\n");
+        printf("      ##\/#/ \||/##/_/##/_#\n");
+        printf("    ###  \/###|/ \/ # ##_###\n");
+        printf("  ##_\_#\_\## | #/###_/_####?#\n");
+        printf(" ## #### # \ #| /  #### ## / ##\n");
+        printf("  __#_--###`  |{,###---###-~\n");
+        printf("            \ }{\n");
+        printf("             }}{\n");
+        printf("             }}{\n");
+        printf("             {{}\n");
+        printf("        , -=-~{ .-^- _\n");
+
+    }
+
+
+void actOne() {
+
+}
+void actTwo() {
+
+}
+void actThree() {
+
+}
+void finalBoss() {
+
+}
---
+++ CSCN71030 - Project 2/Storyline.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdio.h>
+
+void storyBegins();
+void actOne();
+void actTwo();
+void actThree();
+void finalBoss();
