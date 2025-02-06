47d32e54b654593b914a1f1a950fbaacbb9b2935
Mromano06 <matbat2205@gmail.com>
2025-02-06 09:50:59 -0500

    First

--- CSCN71030 - Project 2/main.c
+++ CSCN71030 - Project 2/main.c
@@ -7,7 +7,7 @@
 // CSCN71030_Section2_Group3 - Main class
 // main menu is the start function
 // might add a start() function to make it look better
-int main(void) {
+int main(int argc, char* argv[]) {
 	srand(time(NULL));	// need this here while testing RNG
 	
 	mainMenu();	// calls main menu function
